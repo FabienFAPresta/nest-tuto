5725ab6242c7a6d38dd17ee0da85737a8110c52c
SMB Edition Squad <smbedition@prestashop.com>
2022-09-20 09:38:05 +0200

    Added : Create queries and mutations about Article

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -5,6 +5,11 @@ import {
   ArticleCreateInput,
   ArticleCreateOutput,
 } from './dtos/article-create.dto';
+import { ArticleDeleteOutput } from './dtos/article-delete.dto';
+import {
+  ArticleUpdateInput,
+  ArticleUpdateOutput,
+} from './dtos/article-update.dto';
 import { Article } from './models/article.model';
 
 @Injectable()
@@ -15,8 +20,33 @@ export class ArticleService {
   ) {}
 
   async createArticle(input: ArticleCreateInput): Promise<ArticleCreateOutput> {
-    const newArticle = this.articleRepository.create(input);
-    const article = await this.articleRepository.save(newArticle);
+    const article = this.articleRepository.create(input);
+    await article.save();
     return { article };
+  }
+
+  async updateArticle(
+    id: Article['id'],
+    input: ArticleUpdateInput,
+  ): Promise<ArticleUpdateOutput> {
+    const article = await this.articleRepository.findOneByOrFail({ id });
+
+    article.title = input.title;
+    article.description = input.description;
+    article.image = input.image;
+    await article.save();
+
+    return { article };
+  }
+
+  async deleteArticle(id: Article['id']): Promise<ArticleDeleteOutput> {
+    const article = await this.articleRepository.findOneByOrFail({ id });
+    await article.remove();
+
+    return { id };
+  }
+
+  async articleList(): Promise<Article[]> {
+    return this.articleRepository.find();
   }
 }
--- src/article/resolvers/article.mutations.resolver.ts
+++ src/article/resolvers/article.mutations.resolver.ts
@@ -1,9 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, ID, Mutation, Resolver } from '@nestjs/graphql';
 import { ArticleService } from '../article.service';
 import {
   ArticleCreateInput,
   ArticleCreateOutput,
 } from '../dtos/article-create.dto';
+import { ArticleDeleteOutput } from '../dtos/article-delete.dto';
+import {
+  ArticleUpdateInput,
+  ArticleUpdateOutput,
+} from '../dtos/article-update.dto';
 import { Article } from '../models/article.model';
 
 @Resolver(Article)
@@ -13,5 +18,18 @@ export class ArticleMutationsResolver {
   @Mutation(() => ArticleCreateOutput)
   async createArticle(@Args('input') input: ArticleCreateInput) {
     return this.articleService.createArticle(input);
+  }
+
+  @Mutation(() => ArticleUpdateOutput)
+  async updateArticle(
+    @Args({ name: 'id', type: () => ID }) id: Article['id'],
+    @Args('input') input: ArticleUpdateInput,
+  ) {
+    return this.articleService.updateArticle(id, input);
+  }
+
+  @Mutation(() => ArticleDeleteOutput)
+  async deleteArticle(@Args({ name: 'id', type: () => ID }) id: Article['id']) {
+    return this.articleService.deleteArticle(id);
   }
 }
